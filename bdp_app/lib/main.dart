1593974153a57bb626368a4b391d6998d285729c
stefan <wurstbrotfan@gmx.de>
2019-08-31 00:04:55 +0200

    Add basic pages and buttons

--- bdp_app/lib/main.dart
+++ bdp_app/lib/main.dart
@@ -3,31 +3,32 @@ import 'package:flutter/services.dart';
 
 void main() => runApp(MyApp());
 
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.deepOrange,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.deepOrange,
+        ),
+        home: HomePage(title: 'BDP Test App'));
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+//Klasse für den Login-Screem
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.title}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -41,43 +42,38 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+
+class _HomePageState extends State<HomePage> {
   int _counter = 0;
 
-  void _incrementCounter() {
+  String _activePage = "LoginPage";
+
+
+  Future _loginButtonPressed() async {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-
+      _activePage = "RoomsPage";
     });
   }
 
-  Future _buttonPressed() async {
-    _incrementCounter();
+  Future _roomsButtonPressed() async {
+    setState(() {
+      _activePage = "ChatPage";
+    });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    const channel = EventChannel('someStream');
-    channel.receiveBroadcastStream().listen((dynamic event) {
-      print('Received event: $event');
-    }, onError: (dynamic error) {
-      print('Received error: ${error.message}');
-    });
+  _backButtonPressed() {
+    if (_activePage == "RoomsPage") {
+      setState((){_activePage = "LoginPage";});
+    } else if (_activePage == "ChatPage") {
+      setState((){_activePage = "RoomsPage";});
+    }
+  }
 
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
+  Widget _LoginPage() {
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
@@ -104,22 +100,103 @@ class _MyHomePageState extends State<MyHomePage> {
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            TextField(maxLines: 4,),
-            Text(
-              'You have pushed the button this many times:',
+            Text("Login Page"),
+            new Row(
+              children: [Text("Username")]
+            )
+            ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _loginButtonPressed,
+        tooltip: 'Login',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+
+  Widget _RoomsPage() {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("roomspage"),
+            TextField(
+              maxLines: 4,
             ),
             Text(
               '$_counter',
-              style: Theme.of(context).textTheme.display1,
+              style: Theme
+                  .of(context)
+                  .textTheme
+                  .display1,
+            ),
+            IconButton(
+                icon: Icon(Icons.arrow_back),
+                onPressed: () { _backButtonPressed();}
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _buttonPressed,
+        onPressed: _roomsButtonPressed,
         tooltip: 'Increment',
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
+
+  Widget _ChatPage() {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("chatpage"),
+            IconButton(
+                icon: Icon(Icons.arrow_back),
+                onPressed: () {
+                  _backButtonPressed();
+                }
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const channel = EventChannel('someStream');
+    channel.receiveBroadcastStream().listen((dynamic event) {
+      print('Received event: $event');
+    }, onError: (dynamic error) {
+      print('Received error: ${error.message}');
+    });
+
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    print(_activePage);
+    if (_activePage == "ChatPage") {
+      return _ChatPage();
+    } else if (_activePage == "RoomsPage") {
+      return _RoomsPage();
+    } else if (_activePage == "LoginPage") {
+      return _LoginPage();
+    }
+  }
 }
+
+
