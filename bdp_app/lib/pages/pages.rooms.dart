c5aa31c16517bbddf7073853458308613907f274
shukon <jo.ma@posteo.de>
2019-08-31 15:43:53 +0200

    Add chat rooms landing page with default users

--- bdp_app/lib/pages/pages.rooms.dart
+++ bdp_app/lib/pages/pages.rooms.dart
@@ -1,52 +1,96 @@
 import 'package:flutter/material.dart';
 
+import 'package:bdp_app/pages/pages.chat.dart';
+
 class RoomsPage extends StatefulWidget {
-  RoomsPage({Key key, this.title}) : super(key: key);
+  RoomsPage({Key key, this.title, this.username}) : super(key: key);
   final String title;
+  final String username;
 
   @override
   _RoomsPageState createState() => _RoomsPageState();
 }
 
 class _RoomsPageState extends State<RoomsPage> {
-  Future _roomsButtonPressed() async {
-    Navigator.pushNamed(context, "chat");
-  }
+  List _chatItems = [
+    Text("Stefan"),
+    Text("Thorsten"),
+    Text("Shuki"),
+    Text("Appsperten")
+  ];
 
-  _backButtonPressed() {
-    setState(() {
-      Navigator.pop(context);
-    });
+  final _tabs = <Widget>[
+    Tab(icon: Icon(Icons.camera_alt)),
+    Tab(text: 'CHATS'),
+    Tab(text: 'STATUS'),
+    Tab(text: 'CALLS'),
+  ];
+
+@override
+Widget build(BuildContext context) {
+  return DefaultTabController(
+      length: _tabs.length,
+      initialIndex: 1,
+      child: Scaffold(
+          appBar: AppBar(
+            title: Text("BDP Test App"),
+            bottom: TabBar(tabs: _tabs),
+          ),
+          // body (tab views)
+          body: TabBarView(
+            children: <Widget>[
+              Text('camera'),
+              ChatList(),
+              Text('status'),
+              Text('calls'),
+            ],
+          )));
+}}
+
+List<Map<String, Object>> getChatData(String user) {
+  return [{
+    'avatar': Icon(Icons.person),
+    'name': 'Stefan',
+    'lastMessage': 'hi there',
+    'lastSeen': '2:53 in the afternoon',
+  }, {
+    'avatar': Icon(Icons.settings),
+    'name': 'Thorsten',
+    'lastMessage': 'hi there',
+    'lastSeen': '2:53 in the afternoon',
+  }, {
+    'avatar': Icon(Icons.cake),
+    'name': 'Shuki',
+    'lastMessage': 'hi there',
+    'lastSeen': '2:53 in the afternoon',
+  }, {
+    'avatar': Icon(Icons.group),
+    'name': 'Appsperten',
+    'lastMessage': 'hi there',
+    'lastSeen': '2:53 in the afternoon',
   }
+  ];
+}
+
+class ChatList extends StatelessWidget {
+  ChatList({Key key, this.username}) : super(key: key);
+  final String username;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("BDP Test App"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text("rooms page"),
-            RaisedButton(
-                child: Text("Abmelden"),
-                onPressed: () {
-                  _backButtonPressed();
-                }),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _roomsButtonPressed,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
+    var _chatItems = getChatData(username);
+
+    return ListView.builder(
+      itemCount: _chatItems.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+            title: Text(_chatItems[index]['name']),
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return ChatPage(/*_chatItems[index]*/); // passing data to chat room
+              }));
+            });
+      },
     );
   }
 }
-
-
-
-
